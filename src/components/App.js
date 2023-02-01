04d2cabac60cb0bedf67b8c223f9683dc70727ec
Romainub87 <106332313+Romainub87@users.noreply.github.com>
2023-02-01 17:54:24 +0100

    debut projet React

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,48 @@
-import logo from '../logo.svg';
-import '../style/App.css';
+import { Fragment, useEffect, useState } from "react";
+import "../style/App.css";
+import { render } from "@testing-library/react";
+import axios from "axios";
 
 function App() {
+  const [infos, setInfos] = useState([]);
+  const [auteur, setAuteur] = useState("Journet");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(
+        "https://www.googleapis.com/books/v1/volumes?q=inauthor:" + auteur
+      );
+
+      setInfos(result.data.items);
+    };
+    fetchData();
+  }, [auteur]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        type="text"
+        onChange={(e) => {
+          setAuteur(e.target.value);
+        }}
+      ></input>
+      <Fragment>
+        {infos != null ? (
+          <ul>
+            {infos.map((item) => (
+              <li key={item.id}>
+                {console.log(item)}
+                <p>
+                  {"Auteur : " +
+                    item.volumeInfo.authors +
+                    " /  Titre : " +
+                    item.volumeInfo.title}
+                </p>
+              </li>
+            ))}
+          </ul>
+        ) : null}
+      </Fragment>
     </div>
   );
 }
